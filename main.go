a5b9f431611968384dfdaab9d2b5a50718fa11b8
PratikKumar125 <pratik.kumar@squareboat.com>
2024-12-29 22:42:05 +0530

    ADD:Exports from main.go

--- main.go
+++ main.go
@@ -2,55 +2,72 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"sync"
 
 	drivers "github.com/PratikKumar125/go-queue/drivers/redis"
 	"github.com/PratikKumar125/go-queue/handler"
 )
 
-func greet() (string, error) {
-    fmt.Println("Hello, World!")
-    return "Hello World", nil
-}
+// func greet() (string, error) {
+//     fmt.Println("Hello, World!")
+//     return "Hello World", nil
+// }
 
-func greet2() (any, error) {
-    // fmt.Println("Hello, World 2222222! I HAVE BEEN DELAYEDDDDD")
-    return "", fmt.Errorf("some error occured")
-}
+// func greet2() (any, error) {
+//     return "", fmt.Errorf("some error occured")
+// }
+
+
+type (
+	DispatchJobStruct    = handler.DispatchJobStruct
+	CreateInputStruct    = handler.CreateInputStruct
+	HandlerStruct        = handler.HandlerStruct
+)
+
+var (
+	NewHandler      = handler.NewHandler
+)
+
+type (
+	RedisClient = drivers.RedisClient
+)
+
+var (
+	InitRedisQueue = drivers.InitRedisQueue
+)
 
 func main() {
-    connectionMap := drivers.ConnectionInputStruct{
-        Addr: "localhost:6379",
-        Password: "",
-        DB: 0,
-        Queue: "prateek",
-    }
-	rClient := drivers.InitRedisQueue(connectionMap)
+    fmt.Println("Hello from Prateek Kumar")
+    // connectionMap := drivers.ConnectionInputStruct{
+    //     Addr: "localhost:6379",
+    //     Password: "",
+    //     DB: 0,
+    //     Queue: "prateek",
+    // }
+	// rClient := drivers.InitRedisQueue(connectionMap)
 
-	var wg sync.WaitGroup
+	// var wg sync.WaitGroup
 
-	rClient.StartWorkers(3, &wg) // concurrency limit for number of workers
-	go rClient.StartQueueListener(&wg)
-    go rClient.StartDelayedQueueListener(&wg)
-	go rClient.StartResultProcessor(&wg)
-    wg.Add(3)
+	// rClient.StartWorkers(3, &wg) // concurrency limit for number of workers
+	// go rClient.StartQueueListener(&wg)
+    // go rClient.StartDelayedQueueListener(&wg)
+	// go rClient.StartResultProcessor(&wg)
+    // wg.Add(3)
 
-    greetJobInputs := handler.CreateInputStruct{
-        JobName: "greet",
-        Queue: "prateek", 
-        Lookup: greet,
-    }
-    greetJobInputs2 := handler.CreateInputStruct{
-        JobName: "greet2",
-        Queue: "prateek", 
-        Lookup: greet2,
-    }
+    // greetJobInputs := handler.CreateInputStruct{
+    //     JobName: "greet",
+    //     Queue: "prateek", 
+    //     Lookup: greet,
+    // }
+    // greetJobInputs2 := handler.CreateInputStruct{
+    //     JobName: "greet2",
+    //     Queue: "prateek", 
+    //     Lookup: greet2,
+    // }
 
 
-    jobsHandler := handler.NewHandler()
-    jobsHandler.CreateJob(greetJobInputs)
-    jobsHandler.CreateJob(greetJobInputs2)
+    // jobsHandler := handler.NewHandler()
+    // jobsHandler.CreateJob(greetJobInputs)
+    // jobsHandler.CreateJob(greetJobInputs2)
 
     // dispatch1 := handler.DispatchJobStruct{
     //     JobName: "greet",
@@ -65,20 +82,20 @@ func main() {
     //     log.Println(err)
     // }
     
-    dispatch2 := handler.DispatchJobStruct{
-        JobName: "greet2",
-        Data: map[string]any{"name": "prateek", "email": "prateek378@gmail.com"},
-        Queue: "prateek",
-        Tries: 2,
-        Delay: -1,
-    }
+    // dispatch2 := handler.DispatchJobStruct{
+    //     JobName: "greet2",
+    //     Data: map[string]any{"name": "prateek", "email": "prateek378@gmail.com"},
+    //     Queue: "prateek",
+    //     Tries: 2,
+    //     Delay: -1,
+    // }
     
-    _, err2 := rClient.DispatchJob(dispatch2)
-    if err2 != nil {
-        log.Println(err2)
-    }
+    // _, err2 := rClient.DispatchJob(dispatch2)
+    // if err2 != nil {
+    //     log.Println(err2)
+    // }
 
-	wg.Wait()
+	// wg.Wait()
 
 
     // originals := storage.DiskStruct{
